6cb27be0ae7325519b40a7b2e7ceead28e97e7aa
Kamran <311.kamran@gmail.com>
2022-12-27 01:23:29 +0400

    Add project files.

---
+++ ProxyDesignPattern/ViewModel/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ProxyDesignPattern.ViewModel;
+
+public class BaseViewModel
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string name = null!)
+    {
+        PropertyChangedEventHandler handler = PropertyChanged!;
+        if (handler != null)
+        {
+            handler(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+}
